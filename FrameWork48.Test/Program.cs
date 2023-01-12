5eabdae63079877c7eb60811d8b2aa03c036d6f5
张龙豪 <Sa123,.0>
2023-01-12 16:25:51 +0800

    清理windows内存工具

---
+++ FrameWork48.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using Masuit.Tools.Hardware;
+using Masuit.Tools.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrameWork48.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int i = 0;
+                var xn = SystemInfo.GetMemoryVData();
+                var wl = SystemInfo.GetMemoryPData();
+                Console.WriteLine($"\r\n物理内存：{wl}");
+                Console.WriteLine("\r\n点击任意键开始清理内存。");
+                Console.ReadLine();
+                Console.WriteLine("清理内存中,请等待。\r\n");                
+                Task.Run(() =>
+                {
+                    while (i == 0)
+                    {
+                        Console.Write(".");
+                        Thread.Sleep(TimeSpan.FromMilliseconds(300));
+                    }
+                });
+
+                var dd = Windows.ClearMemory();
+                i++;
+                Console.WriteLine($"\r\n\r\n内存清理完成，当前内存使用率为：{dd}%");
+            }
+        }
+    }
+}
